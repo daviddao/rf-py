967e4abead4e649ac4c015ee5f4881c969cd60f3
daviddao <contact.daviddao@gmail.com>
2015-05-28 16:46:54 +0200

    dropset extraction

--- dropset.py
+++ dropset.py
@@ -1,32 +1,50 @@
 from parse import *
-
+import time
 
 def main():
+  
   # Read the bips file from RAxML
   [trees,mxtips,n_tree] = read_bips("bips.txt")
-
+  
+  start = time.time()
+  # store all dropsets in a file called sets  
+  f = open("drops.txt","w")
+  
   # Iterate through all trees
   for i in trees.keys():
     print("calculating dropset for tree",i)
     # Extract the bips
     s_bips = trees[i]['s_bips']
     ind_bips = trees[i]['ind_bips'] 
+    s_treeList = trees[i]['sTreeList']
     count = 0 
     maxcount = len(s_bips) * len(ind_bips)
-    for s_bip in s_bips:
+    for s_id, s_bip in enumerate(s_bips):
       # sanity check that we have unambigious bitvector representation
       assert(s_bip[0] == False)
-    
-      for ind_bip in ind_bips:
+      
+      # look at all possible combinations of ind_bip and s_bip and calculate the dropset
+      for ind_id, ind_bip in enumerate(ind_bips):
         # second sanity check
         assert(ind_bip[0] == False)
         count = count + 1
         print("looking at",count,"/",maxcount)
         # calculate dropsets
-        indices = calculateDropSet(ind_bip,s_bip)      
+        indices = calculateDropSet(ind_bip,s_bip)
+        # get the global representation of the dropset       
+        drop = [s_treeList[i] for i in indices]
+        # sort the dropset for unique representation
+        drop = sorted(drop)
+        # save tree index and s_bip, ind_bip index
+        f.write(str(i) + " " + str(s_id) + " " + str(ind_id) + "\n")
+        f.write(str(drop) + "\n")
+        
+                
          
   print("Ok, everything's fine")
-
+  f.close()
+  end = time.time()
+  print("Total time needed:",end-start)
 '''
 Function calculating dropset of two bipartitions in bitvector format
 '''
